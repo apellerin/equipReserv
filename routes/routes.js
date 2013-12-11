d564e7bc08a2f379b0891d00fb0c658ebd41a31f
Anthony Pellerin <anthonyleepellerin@hotmail.com>
2013-12-10 18:05:13 -0800

    Built User Registration

--- routes/routes.js
+++ routes/routes.js
@@ -4,20 +4,35 @@ module.exports = function(app){
 
      //User Routes
     app.namespace('/users', function(req, res) {
+
+        //User Authentication/Login
         app.post('/authenticate',function(req,res) {
             users.authenticate(req, res)
               });
-        });
+        
             
+         //User Registration
+        app.post('/register',function(req,res) {
+            users.register(req, res)
+        });
+      
+    });//End Users Namespace
     
-    
+
+
+
+
+
+
+
+
     
     //Catch all requests and verify login state.
     app.all('*',function(req,res,next){
         users.isLoggedIn(req, 
             function(result){
                 if (result) {
-                next();
+                    res.render('index',{title: 'Welcome'});
                 }else{
                     res.render('login',{title : 'Login'});
                 }
