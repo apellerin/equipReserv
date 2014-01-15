decff5b17644dc35151a3190d286400c75ee6eb9
Anthony Pellerin <anthonyleepellerin@hotmail.com>
2014-01-14 17:48:14 -0800

    Replaced form validation library (customized version of jqBootstrapValidation)

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -4,13 +4,13 @@
     $(".list-group-item[href='"+ path + "']").addClass("hover");
 
     //autofocus first field with autofocus attribute
-    $(this).find("[autofocus]:first").focus();
+    //$(this).find("[autofocus]:first").focus();
     
     //toggle for sidebar in smaller viewports
     $('[data-toggle=offcanvas]').click(function() {
         $('.row-offcanvas').toggleClass('active');
     });
-    
+   
     //On modal set focus in first input
     $("#myModal").on('shown.bs.modal', function() {
         $(this).find("[autofocus]:first").focus();
